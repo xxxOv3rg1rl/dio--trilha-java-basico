3fef38a4b2b3ed4300ab754102b3876eb5f456f7
xxxOv3rg1rl <sthefanynatalysouzaribeiro@gmail.com>
2024-05-03 17:58:58 -0300

    adicionando tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,24 @@
-public class App {
+public class TiposVariaveis {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+
+        String meuNome = "Fulano";
+        
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        
+
     }
 }
