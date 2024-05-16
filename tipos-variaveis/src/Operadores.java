0adb428e35d912c17532f363f3f7e30259606099
xxxOv3rg1rl <sthefanynatalysouzaribeiro@gmail.com>
2024-05-16 15:57:30 -0300

    adicionando operadores aritimeticos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,8 +1,23 @@
+// public class Operadores {
+    
+//     public static void main(String[] args) {
+//         String nomeCompleto = "LINGUAGEM" + "JAVA";
+        
+//         System.out.println(nomeCompleto);
+//     }
+// }
+
 public class Operadores {
     
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-        
-        System.out.println(nomeCompleto);
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = + numero * -1;
+
+        System.out.println(numero);
     }
 }
