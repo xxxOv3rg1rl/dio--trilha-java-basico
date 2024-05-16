fe37beb931632df9fba81a0503a314688bdaa0ce
xxxOv3rg1rl <sthefanynatalysouzaribeiro@gmail.com>
2024-05-16 16:19:44 -0300

    adicionando operadores ternarios

---
+++ tipos-variaveis/src/OperadoresTernarios.java
@@ -0,0 +1,15 @@
+// /*EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA SIMPLIFICADA DE IF/ELSE
+public class OperadoresTernarios {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b = 6;
+        String resultado = a==b ?"verdadeiro" : "falso";
+        
+         
+      
+        System.out.println(resultado);
+
+    }
+}   
