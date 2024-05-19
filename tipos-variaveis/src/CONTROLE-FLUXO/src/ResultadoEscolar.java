a1cec84532b9eab92a6646edd3e84db0e531d848
xxxOv3rg1rl <sthefanynatalysouzaribeiro@gmail.com>
2024-05-19 14:54:09 -0300

    exercicios base de entendimento de estruturas condicionais

---
+++ tipos-variaveis/src/CONTROLE-FLUXO/src/ResultadoEscolar.java
@@ -0,0 +1,8 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 7;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
+//Exemplo de condicional composta simples
